0173a1a33ff2f7fc9c60f873e19fc35e07e89540
Merii <mcabezasbanegas@gmail.com>
2024-02-19 21:13:33 +0100

    ModifNRC

--- ClaseC.java
+++ ClaseC.java
@@ -32,6 +32,7 @@ public class ClaseC {
 	public String toString() {
 		return "ClaseC [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + "]";
 	}
+	
 	public void ingresarNombre() {
 		
 	}
