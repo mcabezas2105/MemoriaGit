3575b07e7e1742449a2e72b42b2a492b000a508c
Merii <mcabezasbanegas@gmail.com>
2024-02-19 15:58:12 +0100

    GetAndSet

--- ClaseA.java
+++ ClaseA.java
@@ -10,5 +10,24 @@ public class ClaseA {
 		this.b = b;
 		this.c = c;
 	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public int getC() {
+		return c;
+	}
+	public void setC(int c) {
+		this.c = c;
+	}
+	
 	
 }
