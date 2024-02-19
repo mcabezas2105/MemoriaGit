f76dabe0c888d9d5453667df785864eb188db39e
Merii <mcabezasbanegas@gmail.com>
2024-02-19 20:29:18 +0100

    ModificacionMB

--- ClaseB.java
+++ ClaseB.java
@@ -8,5 +8,12 @@ public class ClaseB {
 	public String toString() {
 		return "ClaseB [a=" + a + ", b=" + b + ", c=" + c + "]";
 	}
+	public ClaseB(double a, double b, double c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
 
 }
