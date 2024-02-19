0925fab53fdb166de6560ec2161275355b76c709
Merii <mcabezasbanegas@gmail.com>
2024-02-19 20:35:01 +0100

    ModificacionNRB

--- ClaseB.java
+++ ClaseB.java
@@ -8,5 +8,24 @@ public class ClaseB {
 	public String toString() {
 		return "ClaseB [a=" + a + ", b=" + b + ", c=" + c + "]";
 	}
+	public double getA() {
+		return a;
+	}
+	public void setA(double a) {
+		this.a = a;
+	}
+	public double getB() {
+		return b;
+	}
+	public void setB(double b) {
+		this.b = b;
+	}
+	public double getC() {
+		return c;
+	}
+	public void setC(double c) {
+		this.c = c;
+	}
+	
 
 }
