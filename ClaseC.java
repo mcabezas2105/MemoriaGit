36ed423a7271fe8522a1feb6cbc80199e7d7366c
Merii <mcabezasbanegas@gmail.com>
2024-02-19 17:05:24 +0100

    ConstructorC

--- ClaseC.java
+++ ClaseC.java
@@ -4,4 +4,11 @@ public class ClaseC {
     String nombre;
     String apellido;
     int edad;
+	public ClaseC(String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+    
 }
